5707f27cc11c823900582be28bdb0904cb24eb9e
lfresco <loryfresco@gmail.com>
2019-02-07 09:51:45 +0100

    Aggiunto un po di cose per i metodi e per gli iteratori

--- c++/BST.h
+++ c++/BST.h
@@ -25,5 +25,52 @@ public:
   BST() {root = nullptr;}
   class Iterator;
   class ConstIterator;
+  
+  BST( const BST& rhs ); // copy costructor
+  BST& operator=(const BST& rhs) ; // copy assignment
 
+
+  BST( const BST&& rhs ); // move costructor
+  BST(const BST&& rhs); // move assignment
+  ~BST();  //destructor
+  
+  // Dato il valore di una key la cerca all'interno dell'albero e ci restituisce
+  // un iterator al nodo.
+  ConstIterator find(const K& item) const;
+
+  Iterator begin() const;
+
+  ConstIterator cbegin() const;
+
+  ConstIterator end() const;
+  ConstIterator begin() const;
+
+  
+
+};
+
+template <class T>
+class BST<K, V>::Iterator : public std::iterator<std::forward_iterator_tag, T> {
+  using Node = BST<K, V>::Node;
+
+    Node* current;
+
+     public:
+       Iterator(Node* n) : current{n} {}
+       Iterator(const Iterator&) = default;
+       T& operator*() const { return current->val; }
+       Iterator& operator++() {
+       		current = current->next.get();
+       		return *this;
+       }
+       
+       Iterator operator++(int) {
+		Iterator it{*this};
+		++(*this);
+		return it;
+      }
+      
+      bool operator==(const Iterator& other) { return current == other.current; }
+      bool operator!=(const Iterator& other) { return !(*this == other); }
+					   
 };
