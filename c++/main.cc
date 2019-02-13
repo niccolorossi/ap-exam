019508a7ba2bd362d8763004a9c64643c4ad6131
Nicola Meneghini <nicolameneghini@hotmail.it>
2019-02-13 10:50:25 +0100

    timing the tree

--- c++/main.cc
+++ c++/main.cc
@@ -4,81 +4,64 @@
 #include <ctime>
 #include <vector>
 #include <cmath>
+#include  <map>
 
 #include "BST.cc"
 
+template <class TM, class K, class V>
+void Timing (std::ofstream& nome_file, TM tree, std::vector<std::pair<K,V>> v){
+
+
+  for (unsigned int i = 0 ; i < v.size(); i+=50)
+  {
+
+   auto start = std::chrono::system_clock::now();
+  for (unsigned int j = 0; j < i; j++)
+        tree.find(v[j].first);
+   auto end = std::chrono::system_clock::now();
+
+  auto el_time = end-start;
+  double log_el_time = log2((std::chrono::duration <double, std::micro> (el_time).count())/double(i));
+
+  if (nome_file)
+    nome_file << i <<" "<<  log_el_time << std::endl;
+
+  }
+
+
+
+}
+
 int main() {
 
-  BST<int,int> myTree;
-  std::chrono::time_point<std::chrono::system_clock> start, end;
   unsigned int Nmax = 10000;
-  std::pair<int,int> pair;
-  std::ofstream before_balance, after_balance, with_map;
+  BST<int,int> myTree;
+  std::map<int,int> myMap;
 
-/*
-  std::pair<int,int> m{10,100};
-  std::pair<int,int> n{11,110};
-  std::pair<int,int> i{1,10};
-  std::pair<int,int> j{5,50};
-  std::pair<int,int> k{18,180};
-
-
-
-  myTree.insert(n);
-  myTree.insert(m);
-  myTree.insert(i);
-  myTree.insert(j);
-  myTree.insert(k);
-*/
-
-
+  std::vector<std::pair<int, int>> v(Nmax);
+  std::ofstream before_balance, after_balance, time_map;
 
   for (unsigned int i = 0; i < Nmax; i++)
   {
-
-    pair.first = i;
-    pair.second = i;
-    myTree.insert(pair);
-
+    v[i].first = i;
+    v[i].second = i;
+    myTree.insert(v[i]);
+    myMap.insert(v[i]);
   }
 
   before_balance.open("before_balance.txt");
-
-  for (unsigned int i = 0 ; i < Nmax; i+=50)
-  {
-
-   start = std::chrono::system_clock::now();
-  for (unsigned int j = 0; j < i; j++)
-        myTree.find(j);
-   end = std::chrono::system_clock::now();
-  auto el_time = end-start;
-
-  if (before_balance)
-    before_balance << i <<" "<< log2((std::chrono::duration <double, std::micro> (el_time).count())/double(i)) << std::endl;
-
-  }
-
+  Timing(before_balance, myTree, v);
   before_balance.close();
 
   myTree.balance();
 
   after_balance.open("after_balance.txt");
-
-  for (unsigned int i = 0 ; i < Nmax; i+=50)
-  {
-   start = std::chrono::system_clock::now();
-  for (unsigned int j = 0; j < i; j++)
-        myTree.find(j);
-  end = std::chrono::system_clock::now();
-  auto el_time = end-start;
-
-  if (after_balance)
-      after_balance << i <<" "<< log2((std::chrono::duration <double, std::micro> (el_time).count())/double(i)) << std::endl;
-
-  }
-
+  Timing(after_balance, myTree, v);
   after_balance.close();
-  //BST<int,int>::Iterator iter = myTree.find(18);
-  //std::cout << (*iter).second << std::endl;
+
+  time_map.open("time_map.txt");
+  Timing(time_map, myMap, v);
+  time_map.close();
+
   return 0;
 }
